e9cacb6f17c8c5b3a4546c0462319a9709433126
gtaou98 <taousanis98@gmail.com>
2025-11-28 00:46:59 +0200

    fix: Add basename to BrowserRouter for GitHub Pages

--- src/main.jsx
+++ src/main.jsx
@@ -6,9 +6,10 @@ import App from './App';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
+  ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <CartProvider>
-      <BrowserRouter>
+      <BrowserRouter basename="/ecommerce-platform">
         <App />
       </BrowserRouter>
     </CartProvider>
