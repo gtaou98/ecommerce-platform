c98cf35d2e400635c6fe3a84c354020ca9a3b9be
gtaou98 <taousanis98@gmail.com>
2025-11-28 00:48:14 +0200

    fix: Correct syntax error in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,6 @@ import App from './App';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <CartProvider>
       <BrowserRouter basename="/ecommerce-platform">
